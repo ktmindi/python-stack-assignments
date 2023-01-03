4182edd3939f98fc8460dd34f139c702d3acdd81
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2023-01-02 23:25:03 -0800

    updated notes

--- review/note.py
+++ review/note.py
@@ -7,7 +7,35 @@ in
 python
 """
 
+#how to print something to the console - in other languages we use console.log()
 print("Hello World!!!")
+
+#data types in python
+
+#boolean
+is_hungry = True
+#numbers
+age = 100
+#strings
+name = "Robo Kat"
+# tuples - data that cannot be modified after creation and can hold a group of mixed values
+dog = ('Charlotte', 'Mini Poodle', 2, 'Brown')
+# to call one of these values we can use
+print(dog[1]) # output will be Mini Poodle
+# lists - CAN BE modified after creation and usually stores a collection of RELATED data
+empty_list=[]
+dogs = ('Charlotte', 'Chloe', 'Chester', 'Tyga', 'Kiwi')
+print(dogs[4]) # output will be Kiwi
+# to change a value in the list 
+dogs[1] = 'Chester'
+dogs[2] = 'Chloe'
+print(dogs) # output: Charlotte, Chester, Chloe, Tyga, Kiwi
+# to add a value to the end 
+dogs.append('Cookie') 
+# to remove a value from the end
+dogs.pop() # will remove cookie
+# to remove a value in the designated location 
+dogs.pop(4) # will remove Kiwi
 
 
 # loop basics
