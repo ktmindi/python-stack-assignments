2d8909b40cab4ba6f6120cae6275d8f839122a05
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2023-02-16 00:12:47 -0800

    explanation of the input output demo

--- fundamentals/oop/monty_python/three_questions.py
+++ fundamentals/oop/monty_python/three_questions.py
@@ -46,3 +46,34 @@ isGuessing = True
 
 while isGuessing == True:
     isGuessing = bridge_keeper()
+
+# When we initially run this file the first thing that appears in our terminal
+# STOP!!!!
+# Those who approach the Bridge of Death must answer me these questions three, ere the other side they see.
+
+#first input is What is your name?
+#second input is What is your quest?
+#third input is one of the three questions at random if this question is what is your favorite color and you answer it with a color that has not been guessed already then we will see
+#Right. Off you go.
+
+#Then it goes back to the beginning of the loop and again will display 
+#STOP
+#Those who approach...
+#first and second input again..
+# third input will be one of the two questions that have yet to pop up. if the question is the velocity question you must answer the correct_answer = "African or European" to get 
+#Wait... I don't know.  AHHHHHHH!!!!!!
+#The bridge keeper was casted into the gorge.
+# and the loop will end
+
+#if answered anything but the correct_answer you will get the response
+#You have been casted into gorge
+
+#if asked the capital of assyria?
+# must answer Ninevah 
+#which will get the response right off you go 
+# and then it will loop again 
+#stop
+#those who approach..
+
+
+# the loop will only end if you get the question about the velocity and answer it correctly
