b230edb6cacd99a0323c1c75a8e7b272b5ea25e6
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2023-02-15 23:53:03 -0800

    demo code

--- test_folder/test.py
+++ test_folder/test.py
@@ -1,6 +1,2 @@
-weekend = {"Sun": "Sunday", "Sat": "Saturday"} #literal notation
-capitals = {} #create an empty dictionary then add values
-capitals["svk"] = "Bratislava"
-capitals["deu"] = "Berlin"
-capitals["dnk"] = "Copenhagen"
-print(capitals)
+favorite_color = input('What is your favorite color? ') # input takes a prompt, which needs to be a string
+print(f'Your favorite color is: {favorite_color}') #output, prints the color given to the console
