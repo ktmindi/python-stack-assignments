e0895eae5812cab14a6582c76fb3723ae9efc286
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2023-02-21 22:08:19 -0800

    Co-authored-by: Kenny Nguyen <kennysghub@users.noreply.github.com>

--- fundamentals/oop/math_dojo.py
+++ fundamentals/oop/math_dojo.py
@@ -1,12 +1,37 @@
 class MathDojo:
     def __init__(self):
-    	self.result = 0
+        self.result = 0
     def add(self, num, *nums):
-    	# your code here
+        self.result += num
+        for n in nums: 
+            self.result += n
+        return self
+
     def subtract(self, num, *nums):
-    	# your code here
+        self.result -= num
+        for n in nums: 
+            self.result -=n 
+        return self
+
 # create an instance:
+
 md = MathDojo()
 # to test:
 x = md.add(2).add(2,5,1).subtract(3,2).result
 print(x)	# should print 5
+
+#   md.add(2)
+#       return instance of MathDojo which is md 
+#       md instance now has a result attribute=2 
+#       md.result = 2
+#   md.add(2,5,1)
+#       return instance of MathDojo 
+#       md.result = 10 
+#       return md 
+#   md.subtract(3,2)
+#       md.result=5 
+#       return md 
+#   x is equal to the instance md invoking these methods,   and then at the end it accesses the result attribute (.result) of md 
+#   So last line is x = md.result
+#   and we know md.result=5 after calling these methods
+
--- test_folder/test.py
+++ test_folder/test.py
@@ -1,5 +1,19 @@
-def manyargs(arg1, *args):
-        for a in args:
-            print(a)
+class MathDojo:
+    def __init__(self):
+        self.result = 0
 
-manyargs("one","two","three")
+    def add(self, num, *nums):
+        self.result+=num
+        for n in nums:
+            self.result+=n
+        
+    def subtract(self, num, *nums):
+        self.result-=num
+        for n in nums:
+            self.result-=n
+
+# create an instance:
+md = MathDojo()
+# to test:
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x)	# should print 5
